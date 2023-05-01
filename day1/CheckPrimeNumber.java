486dae2d9839ec41d6ea5c1d852f5b791e1a5f64
Lavanyamp17 <132283409+Lavanyamp17@users.noreply.github.com>
2023-05-01 13:46:01 -0400

    Add files via upload

---
+++ day1/CheckPrimeNumber.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class CheckPrimeNumber {
+
+	public static void main(String[] args) {
+		
+		int num = 13;
+		boolean primeNum = true;
+		
+		for (int i = 2; i <num; i++) {
+			if (num%i==0) {
+				primeNum = false;
+				break;
+			}
+		}
+			if (primeNum) {
+				System.out.println("Number is Prime");
+			}
+			else {
+				System.out.println("Number is not Prime");
+			}
+				
+		}
+
+	
+
+}
