c50cccfa0106e8433ea1e44d9ba85c23eb24bb29
Lavanyamp17 <132283409+Lavanyamp17@users.noreply.github.com>
2023-05-15 11:44:12 -0400

    Add files via upload

---
+++ week3/day1/org/college/College.java
@@ -0,0 +1,17 @@
+package org.college;
+
+public class College {
+
+	public void collegeName()
+	{
+		System.out.println("College Name --> College");
+	}
+	public void collegeCode()
+	{
+		System.out.println("College Code --> College");
+	}
+	public void collegeRank()
+	{
+		System.out.println("College Rank --> College");
+	}
+}
