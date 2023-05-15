812af3f7ec4dfeb4a4b6d798ea7d37b1d4959a8f
Lavanyamp17 <132283409+Lavanyamp17@users.noreply.github.com>
2023-05-15 10:55:51 -0400

    Add files via upload

---
+++ BankInfo.java
@@ -0,0 +1,17 @@
+package week3.day1;
+
+public class BankInfo {
+
+	public void saving()
+	{
+		System.out.println("Savings --> BankInfo");
+	}
+	public void fixed()
+	{
+		System.out.println("Fixed --> BankInfo");
+	}
+	public void deposit()
+	{
+		System.out.println("Deposit --> BankInfo");
+	}
+}
